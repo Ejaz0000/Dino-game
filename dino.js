5b91f8fb9061cca88498fdb9830af6a5177b08ea
Ejaz0000 <ejahmed999@gmail.com>
2023-06-01 00:38:50 +0600

    added jumping

--- dino.js
+++ dino.js
@@ -15,12 +15,17 @@ const FRAME_TIME = 100
 let isJumping
 let dinoFrame
 let currentFrameTime
+let yVelocity
 
 export function setupDino(){
 
     isJumping = false
     dinoFrame = 0
     currentFrameTime = 0
+    yVelocity = 0
+  setCustomProperty(dinoElem, "--bottom", 0)
+  document.removeEventListener("keydown", onJump)
+  document.addEventListener("keydown", onJump)
 
 }
 
@@ -31,8 +36,28 @@ export function updateDino(delta,speedScale){
 
 }
 
-function handleJump(){
+function handleJump(delta){
 
+    if (!isJumping) return
+
+    incrementCustomProperty(dinoElem, "--bottom", yVelocity * delta)
+
+    if (getCustomProperty(dinoElem, "--bottom") <= 0) {
+        setCustomProperty(dinoElem, "--bottom", 0)
+        isJumping = false
+      }
+
+    yVelocity -= GRAVITY * delta
+
+}
+
+function onJump(e) {
+
+    if (e.code !== "Space" || isJumping) return
+
+
+    yVelocity = JUMP_SPEED
+    isJumping = true
 }
 
 function handleRun(delta,speedScale){
